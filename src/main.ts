e14f7e1719493dfc4e3068807afcfc5dbef85d33
MylesAndersson140 <134179125+MylesAndersson140@users.noreply.github.com>
2024-10-22 19:17:56 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,14 @@ const APP_NAME = "Hello World!";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+app.innerHTML = `
+    <h1>${APP_NAME}</h1>
+    <canvas id="canvas" width="256" height="256"></canvas>
+`;
+
+//Drew inspiration from line 4, as code was showing warnings previously.
+const canvas = document.querySelector<HTMLCanvasElement>("#canvas")!;
+const ctx = canvas.getContext("2d")!;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
